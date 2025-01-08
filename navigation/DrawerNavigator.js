55f755beec8a791756a94d139f97cf77aeb6264d
JCUYLLE2 <r0887914@student.thomasmore.be>
2025-01-08 13:37:29 +0100

    styling nav

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -10,7 +10,25 @@ const Drawer = createDrawerNavigator();
 const DrawerNavigator = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#2E282A', // Donkerdere kleur voor de header
+          },
+          headerTintColor: '#FFD9DA', // Tekstkleur in de header
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          drawerStyle: {
+            backgroundColor: '#191516', // Achtergrond van de drawer
+          },
+          drawerActiveTintColor: '#FFD9DA', // Kleur van de actieve itemtekst
+          drawerInactiveTintColor: '#EB638B', // Kleur van de inactieve itemtekst
+          drawerLabelStyle: {
+            fontSize: 16,
+          },
+        }}
+      >
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Search" component={SearchScreen} />
         <Drawer.Screen name="Details" component={DetailsScreen} />
