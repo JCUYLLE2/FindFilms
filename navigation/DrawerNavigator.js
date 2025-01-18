ae33f8927a3b10911689c3d4e3a9ec651c2c0ca6
JCUYLLE2 <r0887914@student.thomasmore.be>
2025-01-18 13:24:01 +0100

    remove detail link

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
-import DetailsScreen from '../screens/DetailsScreen';
 import SearchScreen from '../screens/SearchScreen';
 
 const Drawer = createDrawerNavigator();
@@ -13,17 +12,17 @@ const DrawerNavigator = () => {
       <Drawer.Navigator
         screenOptions={{
           headerStyle: {
-            backgroundColor: '#2E282A', // Donkerdere kleur voor de header
+            backgroundColor: '#2E282A',
           },
-          headerTintColor: '#FFD9DA', // Tekstkleur in de header
+          headerTintColor: '#FFD9DA',
           headerTitleStyle: {
             fontWeight: 'bold',
           },
           drawerStyle: {
-            backgroundColor: '#191516', // Achtergrond van de drawer
+            backgroundColor: '#191516',
           },
-          drawerActiveTintColor: '#FFD9DA', // Kleur van de actieve itemtekst
-          drawerInactiveTintColor: '#EB638B', // Kleur van de inactieve itemtekst
+          drawerActiveTintColor: '#FFD9DA',
+          drawerInactiveTintColor: '#EB638B',
           drawerLabelStyle: {
             fontSize: 16,
           },
@@ -31,7 +30,6 @@ const DrawerNavigator = () => {
       >
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Search" component={SearchScreen} />
-        <Drawer.Screen name="Details" component={DetailsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
